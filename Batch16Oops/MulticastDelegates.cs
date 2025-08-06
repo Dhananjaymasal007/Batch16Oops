0b3ab3cddd7ce08ed99594f39b1e459be9571cfd
Dhananjay Masal <dhananjay9096@gmail.com>
2025-08-06 08:04:18 +0530

    Delegates emample

---
+++ Batch16Oops/MulticastDelegates.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch16Oops
+{
+
+
+    public delegate void Nofication();
+
+
+    class MDelegate
+    {
+        public void StartingProcess()
+        {
+            Console.WriteLine("Process Started...........");
+        }
+        public void EndingProcess()
+        {
+            Console.WriteLine("Process Ended........");
+
+        }
+    }
+    internal class MulticastDelegates
+    {
+        static void Main()
+        {   
+             // class object
+             MDelegate MD = new MDelegate();
+
+
+            //create the delegate instance
+
+            Nofication Notify = MD.StartingProcess;
+            MD.StartingProcess();
+
+            Notify += MD.EndingProcess;
+            MD.EndingProcess();
+
+            Notify +=() => Console.WriteLine("All process are stopped");
+            Notify();
+
+        }
+
+    }
+}
