5f4414846d07cd08e6c6def1802b5cf083074d55
Mayur Doke <dokemayur95@gmail.com>
2025-08-08 08:22:40 +0530

    out example

---
+++ Batch16Oops/OutExample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch16Oops
+{
+    internal class OutExample
+    {
+        static void Main()
+        {
+            int a = 10;
+            string name = "Mayur";
+            IntializeValue(out a,out name);
+            Console.WriteLine(a);
+
+        }
+        public  static void IntializeValue(out int a, out string  name);
+        {
+
+        }
+    }
+}
