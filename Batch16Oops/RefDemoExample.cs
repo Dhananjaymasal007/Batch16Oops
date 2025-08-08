9f5b2faa9ca79fdcad87485c4a4e953515b52c29
Mayur Doke <dokemayur95@gmail.com>
2025-08-08 11:15:52 +0530

    ref demo example

---
+++ Batch16Oops/RefDemoExample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch16Oops
+{
+
+    internal class RefDemoExample
+    {
+        public static  void IncrementValue(ref int incvalue)
+        {
+            incvalue++;
+        }
+
+        static void Main()
+        {
+            
+            int incvalue = 10;
+            Console.WriteLine($"before increment value:{incvalue}");
+
+            IncrementValue(ref incvalue);
+            Console.WriteLine($"after incerment value:{incvalue}");
+
+
+
+        }
+    }
+}
